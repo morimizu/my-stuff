4672b311c5a6f227852e40a13e081b9e3f6326a1
Ben <Ben@192.168.0.13>
2018-02-17 10:51:41 -0400

    init

---
+++ IMGPricing/IMGPricing/src/main/java/com/example/demo/ImgPricingApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgPricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgPricingApplication.class, args);
+	}
+}
